59f8b913a1466223a3a88675aa51c1a125fa17fa
betoab728 <betoab728@gmail.com>
2024-10-15 13:54:12 -0500

    api crud lista

--- WebApiReportes/Models/Cargo.cs
+++ WebApiReportes/Models/Cargo.cs
@@ -1,5 +1,8 @@
-﻿namespace WebApiReportes.models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiReportes.models
 {
+    [Table("cargo")]  // Especifica que la tabla en la BD es 'cargo' en minúsculas
     public class Cargo
     {
         //la tbla cargo tiene los campos: id,nombre,created_at,updated_at
